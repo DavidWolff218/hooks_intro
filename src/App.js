ac3cefd6f29f70e95187043d5169c1bfebfa24da
doodleman218 <dcwolff218@gmail.com>
2020-10-09 16:33:33 -0500

    fix uuid install and used in sample array

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css'
 import {ExpenseForm} from './components/ExpenseForm';
 import {ExpenseList} from './components/ExpenseList';
 import {Alert} from './components/Alert'
+import uuid from 'react-uuid'
 
+const initialExpenses = [
+  {id:uuid(), charge: "rent", amount:1600},
+  {id:uuid(), charge: "car payment", amount:400},
+  {id:uuid(), charge: "credit card bill", amount:1200}
+]
+console.log(initialExpenses)
 
 function App() {
   return (
