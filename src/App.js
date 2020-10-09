9c82bd2fdbd8862686e131409b8af18e546fbd71
doodleman218 <dcwolff218@gmail.com>
2020-10-09 16:08:47 -0500

    app.js rendering components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css'
+import {ExpenseForm} from './components/ExpenseForm';
+import {ExpenseList} from './components/ExpenseList';
+import {Alert} from './components/Alert'
 
 
 function App() {
   return (
-    <div >
-      hihihihihih
-    </div>
+    <>
+    <Alert></Alert>
+    <ExpenseForm></ExpenseForm>
+    <ExpenseList></ExpenseList>
+      
+
+    </>
   );
 }
 
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import {ExpenseItem} from './ExpenseItem'
 
 export const ExpenseList = () => {
   return (
     <div>
       hello from expense list
+      <ExpenseItem />
     </div>
   )
 }
