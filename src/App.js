5a95db177dbebbea172d5bbe1e7c63a3c2395ea3
doodleman218 <dcwolff218@gmail.com>
2020-10-11 10:24:08 -0500

    import and basic setup of useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css'
 import {ExpenseForm} from './components/ExpenseForm';
 import {ExpenseList} from './components/ExpenseList';
@@ -10,16 +10,16 @@ const initialExpenses = [
   {id:uuid(), charge: "car payment", amount:400},
   {id:uuid(), charge: "credit card bill", amount:1200}
 ]
-console.log(initialExpenses)
 
 function App() {
+  const [expenses, setExpenses] = useState(initialExpenses)
+ 
   return (
     <>
     <Alert></Alert>
+    <h1>budget calculator</h1>
     <ExpenseForm></ExpenseForm>
     <ExpenseList></ExpenseList>
-      
-
     </>
   );
 }
