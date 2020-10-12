e00dde5aac1ca52539116c1834bdabbafaf4f775
doodleman218 <dcwolff218@gmail.com>
2020-10-12 11:23:43 -0500

    create state change funtions

--- src/App.js
+++ src/App.js
@@ -12,8 +12,23 @@ const initialExpenses = [
 ]
 
 function App() {
+  // **** state values *****
   const [expenses, setExpenses] = useState(initialExpenses)
- 
+  // single expense
+  const [charge, setCharge] = useState('')
+  // single amount
+  const [amount, setAmount] = useState('')
+   // **** functionality *****
+   const handleCharge = e => {
+     setCharge(e.target.value)
+   }
+   const handleAmount = e => {
+    setAmount(e.target.value)
+  }
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
+
   return (
     <>
     <Alert></Alert>
